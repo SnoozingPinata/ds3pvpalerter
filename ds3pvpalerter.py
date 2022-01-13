59bc888ebd3cf491a86848bf8d68da0609968a15
Samuel Melton <smelton8720@gmail.com>
2022-01-13 17:30:33 -0600

    text alert working, image detector not

--- ds3pvpalerter.py
+++ ds3pvpalerter.py
@@ -1,13 +1,32 @@
+from mimetypes import init
+from charset_normalizer import detect
 import pyautogui
-import twilio
+import keys
+import twilio.rest
+# twilio.rest.client
 
 
-
-if __name__ == "__main__":
+def invasion_detected():
     while True:
         try: 
             # speed this up by passing regions to check based on screen width and height
-            invading_image = pyautogui.locateOnScreen('invading.png')
+            invading_image = pyautogui.locateOnScreen('invading.png', grayscale=True, region=(0,0,400,400))
         except pyautogui.ImageNotFoundException:
-            pass
-        print("found?")
+            break
+    return True
+
+def send_invasion_alert(client):
+    client.messages.create(
+        body="Invasion Found!!!",
+        from_=keys.twilio_number,
+        to=keys.target_number
+    )
+
+
+if __name__ == "__main__":
+    client = twilio.rest.Client(keys.account_sid, keys.auth_token)
+    if invasion_detected():
+        print("True")
+        #send_invasion_alert(client)
+    else:
+        print("False")
