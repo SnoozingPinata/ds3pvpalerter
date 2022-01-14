a77920f6667927b1a5112d1afebcbfe6b7e82408
Samuel Melton <smelton8720@gmail.com>
2022-01-14 12:08:24 -0600

    polishing first working version.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ds3pvpalerter
 Sends a text message to your cell alerting you of pvp events in dark souls 3.
+(This project is still in a very early stage and requires some technical knowledge to install/setup.)
+
+Installation:
+    Install python 3.10 and the following packages:
+        pyautogui
+        twilio-python
+
+How to use:
+    Create a twilio trial account here: https://www.twilio.com/
+    Add a trial phone number to your account.
+    Create a file named "keys.py" in the folder these files are in.
+    Open the "keys.py" file you created and copy the following 4 lines into it:
+        account_sid = ''
+        auth_token = ''
+        twilio_number = ''
+        target_number = ''
+    Remove any indentation in the keys.py file.
+    Find your account sid, auth token, and twilio number within your twilio account page.
+    Copy each of these strings into the keys.py file between the single quotationi marks (').
+    Enter your target number (with country code) into the target_number field in keys.py.
+    Verify the monitor you are playing Dark Souls 3 on is your Operating System's "main" monitor.
+    Run ds3pvpalerter.py on your second monitor.
+    You will see log messages when events occur including a message when the program has started.
--- ds3pvpalerter.py
+++ ds3pvpalerter.py
@@ -6,13 +6,25 @@ import keys
 import twilio.rest
 import time
 
+
+# Important to get the region to search for because it drastically speeds up the search time.
+# The in-game notification will blink off before the program can find it if this is not used.
 def get_search_region():
+    """Return the appropriate search region based on resolution of monitor."""
+    # Will update this with a dictionary mapping different resolutions in the future. Probably.
+    # 2560x1440p is the only resolution that works currently.
+    # May need to figure out a way to get resolution of game window instead of monitor.
     screen_width, screen_height = pyautogui.size()
     wqhd_region = (675, 965, 600, 10)
     if screen_width == 2560 and screen_height == 1440:
         return wqhd_region
 
+# Searches the given region for the "pvp_notification_bar.png" image in same directory as program.
+# This image just matches the top left part of the notification banner's "bar".
+# Couldn't find any other messages appearing in this zone with this bar besides PVP events.
+# Likely can speed this up by just testing pixels in a line where the banner bar should be based on resolution.
 def invasion_detected(search_region):
+    """Return true if PVP Event Notification is currently on the screen."""
     val = pyautogui.locateOnScreen(image='pvp_notification_bar.png', region=search_region, grayscale=True, confidence=0.8)
     if val != None:
         print(val)
@@ -21,6 +33,7 @@ def invasion_detected(search_region):
         return False
 
 def send_invasion_alert(client):
+    """Send a text message indicating a PVP Event has been detected with the given Twilio rest client object."""
     client.messages.create(
         body="PVP Event Detected!",
         from_=keys.twilio_number,
@@ -29,11 +42,16 @@ def send_invasion_alert(client):
 
 
 if __name__ == "__main__":
+    # Print out Just for logging purposes to know it's working and to verify number.
     print(f"Starting DS3 PvP Alerter!\nTarget Number is {keys.target_number}")
     search_region = get_search_region()
     twilio_client = twilio.rest.Client(keys.account_sid, keys.auth_token)
     while True:
         if invasion_detected(search_region):
+            # Print out for logging and testing purposes.
             print("PVP Event Detected!")
             send_invasion_alert(twilio_client)
+            # Timeout is needed because notification pops up 3 times for many frames.
+            # Program will send a huge amount of text messages without this for a single invasion.
+            # 45 seconds *should* be short enough to catch a second pvp event, might need to shorten.
             time.sleep(45)
